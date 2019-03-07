aab4320c7351d2c0edeb386efc6dff2baa05d42a
maikeru2020 <maikeru2020@gmail.com>
2019-03-06 20:04:34 -0800

    port corrected

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000;
-//const port = process.env.PORT
+//const port = 5000;
+const port = process.env.PORT
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log('Server on port ' + port))
